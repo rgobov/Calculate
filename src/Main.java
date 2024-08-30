aceef854b5c942931f7bb283d5bce6b7badb77db
rgobov <rgobov@gmail.com>
2024-08-30 21:40:06 +0300

    final1

--- src/Main.java
+++ src/Main.java
@@ -24,9 +24,9 @@ public class Main {
     // Объявляем переменную для знака операции
     public static char operation;
 
-    // Объявляем строку для первой части
+    // Объявляем строку для первой части сложения-вычитания
     public static String firstPart;
-    // Объявляем строку для второй части
+    // Объявляем строку для второй части сложения-вычитания
     public static String secondPart;
 
     // Определяем знак операации.
@@ -45,19 +45,29 @@ public class Main {
         boolean isoperation = false;
         for (String c : simbolsOfOperation) {
             if (phrase.contains(c)) {
-                int index = phrase.indexOf(c);
-                firstPart = phrase.substring(1, index - 1);
-                secondPart = phrase.substring(index + 2, phrase.length() - 1);
-                isoperation = true;
+                if (c.equals(simbolsOfOperation[0]) || c.equals(simbolsOfOperation[1])) {
+                    int index = phrase.indexOf(c);
+                    firstPart = phrase.substring(1, index - 1);
+                    secondPart = phrase.substring(index + 2, phrase.length() - 1);
+                    isoperation = true;
+                    break;
+                }
+                if (c.equals(simbolsOfOperation[2]) || c.equals(simbolsOfOperation[3])) {
+                    int index = phrase.indexOf(c);
+                    firstPart = phrase.substring(1, index - 1);
+                    secondPart = phrase.substring(index + 1);
+                    isoperation = true;
+                    break;
+                }
             }
-            ;
+
 
         }
 
         if (!isoperation) {
             throw new NoSuchOperationException("Нет допустимой операции");
         }
-        if (firstPart.length() + secondPart.length() > 20) {git
+        if (firstPart.length() + secondPart.length() > 20) {
             throw new StringMoreThen10("Длинна аргумента больше 10знаков");
         }
     }
@@ -67,7 +77,9 @@ public class Main {
         for (char c : a.toCharArray()) {
             if (Character.isDigit(c)) {
                 digit = true;
-            }else {digit = false;}
+            } else {
+                digit = false;
+            }
         }
         if (digit) throw new FirstStringMustBeString("Первый аргумент должен быть строкой");
     }
